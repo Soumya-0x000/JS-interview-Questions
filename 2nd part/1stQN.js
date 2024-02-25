3f3336f14e9439dc59e243bc779491e87749b851
Soumya-0x000 <soumyadas429@gmail.com>
2024-02-25 22:41:45 +0530

    2nd part 1stQN updated

--- 2nd part/1stQN.js
+++ 2nd part/1stQN.js
@@ -2,12 +2,22 @@ function SSD() {
     console.log('SSD')
 }
 
+async function createData() {
+    return 1
+}
+
 setTimeout(() => {
     console.log('B')
 }, 2000);
 
 SSD()
 
+createData().then((data) => {
+    console.log('Got the data', data)
+})
+
 console.log('C')
 
 Promise.resolve().then(() => console.log('D'))
+
+console.log('E')
