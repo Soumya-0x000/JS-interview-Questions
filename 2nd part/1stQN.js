cb3b5e0769b9bfd702e2011fdd0df1942aac64de
Soumya-0x000 <soumyadas429@gmail.com>
2024-02-26 13:20:57 +0530

    2nd part 1stQN updated

--- 2nd part/1stQN.js
+++ 2nd part/1stQN.js
@@ -3,21 +3,28 @@ function SSD() {
 }
 
 async function createData() {
-    return 1
-}
+    let status = undefined
+    try {
+        const res = await fetch('https://api.github.com/users/Soumya-0x000')
+        status = res.status
+    } catch (error) {
+        console.error('error')
+    }
+    return status
+}  //Micro task queue has high priority
 
 setTimeout(() => {
     console.log('B')
-}, 2000);
+}, 1000);    //callback queue / task queue has low priority
 
 SSD()
 
-createData().then((data) => {
-    console.log('Got the data', data)
+createData().then((code) => {
+    console.log('Status code: ', code)
 })
 
 console.log('C')
 
-Promise.resolve().then(() => console.log('D'))
+Promise.resolve().then(() => console.log('D'))    //Micro task queue has high priority
 
 console.log('E')
